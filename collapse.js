9f9054744a06a051675eca63e0745d81b3266bc5
pgoggijr <petergoggijr@gmail.com>
2015-05-19 13:16:33 -0400

    button now gets added, doesn't yet do anything

--- collapse.js
+++ collapse.js
@@ -1,8 +1,19 @@
 comments = [];
-rows = document.getElementsByClassName("athing");
-console.log(document.getElementsByClassName("athing"));
+rows = document.getElementsByClassName("comhead");
+//console.log(document.getElementsByClassName("comhead"));
 
-for(var i = 0; i < rows.length; i++) {
-	item = rows[i].insertCell(1); 
-	item.innerHTML = "Hi";
-}
+rows = Array.prototype.filter.call(rows, function (item) {
+	if(item.className == "comhead") {
+		return item;
+	}
+});
+
+rows.forEach(function (item) {
+	var swag = document.createElement("SPAN");
+	var textNode = document.createTextNode("[+]");
+	swag.appendChild(textNode);
+	item.insertBefore(swag, item.childNodes[0]);
+});
+
+//debug
+console.log(rows);
